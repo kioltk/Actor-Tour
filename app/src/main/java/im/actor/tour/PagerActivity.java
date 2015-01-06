37c237e77c13b0862fdcf758e71832c7d2feb90d
David <kioltk@gmail.com>
2015-01-06 15:59:42 +0200

    Initial commit

--- app/src/main/java/im/actor/tour/PagerActivity.java
+++ app/src/main/java/im/actor/tour/PagerActivity.java
@@ -19,21 +19,38 @@ public class PagerActivity extends ActionBarActivity {
 
 
         final VerticalViewPager viewPager = (VerticalViewPager) findViewById(R.id.viewpager);
-        final TextView status1 = (TextView) findViewById(R.id.status1);
-        final TextView status2 = (TextView) findViewById(R.id.status2);
-        final TextView status3 = (TextView) findViewById(R.id.status3);
+        //final TextView status1 = (TextView) findViewById(R.id.status1);
+        //final TextView status2 = (TextView) findViewById(R.id.status2);
+        //final TextView status3 = (TextView) findViewById(R.id.status3);
 
-        final View firstContent = findViewById(R.id.first_content);
-        final View lastContent = findViewById(R.id.last_content);
+        final View backToTopText = findViewById(R.id.back_to_top);
+        final View backToTopArrow = findViewById(R.id.back_to_top_arrow);
 
+        final View paralax =  findViewById(R.id.paralax);
+        final View paralaxImage =  findViewById(R.id.paralax_image);
 
+        final View loginHolder = findViewById(R.id.login_holder);
+        final View welcomeImage = findViewById(R.id.welcome_logo);
+        final View welcomeText = findViewById(R.id.welcome_text);
+
+        View.OnClickListener jumpToTopListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backToTopArrow.animate().alpha(0).setDuration(0).start();
+                backToTopText.animate().alpha(0).setDuration(0).start();
+                viewPager.setCurrentItem(1, true);
+            }
+        };
+        backToTopText.setOnClickListener(jumpToTopListener);
+        backToTopArrow.setOnClickListener(jumpToTopListener);
 
         viewPager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
             @Override
-            public Fragment getItem(int i) {
-                if(i == 0 || i == 4)
+            public Fragment getItem(int poistion) {
+                if(poistion == 0 || poistion == 4)
                     return new Fragment();
-                return new TourFragment();
+
+                return TourFragment.getInstance(poistion);
             }
 
             @Override
@@ -53,27 +70,100 @@ public class PagerActivity extends ActionBarActivity {
         viewPager.setClipChildren(false);
 
         viewPager.setOnScrollListener(new ScrollListener() {
+            public static final int LOGINHOLDER_STATUS_OUT = 1;
+            public static final int LOGINHOLDER_STATUS_VISIBLE = 2;
+            public int loginHolderStatus;
+            public boolean welcomeShowed = false;
+            public boolean mainHided = false;
+
             @Override
             public void onScroll(int y, float page) {
-                status3.setText("\ny: " + y + "\nyOffset: " + page);
+                //status3.setText("\ny: " + y + "\nyOffset: " + page);
 
-                if (page < 0.5) {
-
+                if (page < 1) {
                     float alpha = 1 - page * 2;
-                    firstContent.setAlpha(alpha);
-                    status1.setText("alpha:" + alpha);
+                    backToTopArrow.setAlpha(alpha);
+                    //status1.setText("alpha:" + alpha);
+                    if(loginHolder.getTop()!=0) {
+                        float loginY = (float) ((float) 1 - (page));
+                        loginHolder.animate().y(loginHolder.getTop() * loginY).setDuration(0).setStartDelay(0).start();
+                        loginHolderStatus = LOGINHOLDER_STATUS_VISIBLE;
+                        float welcomeImageY = ((float) 1.0 - (page * 4));
+                        //welcomeImage.animate().y(welcomeImage.getTop() * welcomeImageY).setDuration(0).setStartDelay(0).start();
 
+                        float welcomeTextY = (float) ((float) 1.0 - (page * 2.5));
+                        if (alpha > 0) {
+                            welcomeImage.animate().alpha(alpha).setDuration(0).start();
+                            welcomeText.animate().alpha(alpha).setDuration(0).start();
+                        } else {
+                            welcomeImage.animate().alpha(0).setDuration(0).start();
+                            welcomeText.animate().alpha(0).setDuration(0).start();
+                        }
+                    }
+                    mainHided = false;
                 }
-
-                if (page > 3.5) {
+                if (page>=1 && page <= 3) {
+                    if(!mainHided) {
+                        mainHided = true;
+                        loginHolder.animate().y(0).setDuration(0).start();
+                        welcomeImage.animate().alpha(0).setDuration(0).start();
+                        welcomeText.animate().alpha(0).setDuration(0).start();
+                        backToTopArrow.animate().alpha(0).setDuration(0).start();
+                        backToTopText.animate().alpha(0).setDuration(0).start();
+                    }
+                }
+                if (page > 3) {
 
                     float alpha = (page - 3) * 2 - 1;
-                    lastContent.setAlpha(alpha);
-                    status1.setText("alpha:" + alpha);
+                    backToTopText.animate().alpha(alpha).setDuration(0).start();
+                    backToTopArrow.animate().alpha(alpha).setDuration(0).start();
+                    float progress = page - 3;
+                    //status1.setText("progress:" + progress);
 
+                    if(progress>0.5){
+                        if(loginHolderStatus == LOGINHOLDER_STATUS_VISIBLE) {
+                            loginHolderStatus = LOGINHOLDER_STATUS_OUT;
+                            loginHolder.animate().y( - loginHolder.getHeight()).setDuration(200).setStartDelay(0).start();
+                        }
+                        welcomeImage.animate().alpha(alpha*2-0.5f).setDuration(0).start();
+                        /*if(progress>0.9) {
+                            if (!welcomeShowed) {
+                                welcomeShowed = true;
+
+                                welcomeImage.setAlpha(1);
+                                welcomeText.setAlpha(1);
+
+                                AnimationSet imageAnimationSet = new AnimationSet(true);
+                                imageAnimationSet.setDuration(300);
+                                imageAnimationSet.setInterpolator(new AccelerateDecelerateInterpolator());
+                                imageAnimationSet.addAnimation(new AlphaAnimation(0, 1));
+                                imageAnimationSet.addAnimation(new ScaleAnimation(0.5f, 1, 0.5f, 1, Animation.RELATIVE_TO_SELF, (float) 0.5, Animation.RELATIVE_TO_SELF, 0.5f));
+                                welcomeImage.startAnimation(imageAnimationSet);
+
+
+                                AnimationSet textAnimationSet = new AnimationSet(true);
+                                textAnimationSet.setDuration(300);
+                                textAnimationSet.setStartOffset(250);
+                                textAnimationSet.setInterpolator(new DecelerateInterpolator());
+                                textAnimationSet.addAnimation(new AlphaAnimation(0, 1));
+                                textAnimationSet.addAnimation(new TranslateAnimation(Animation.RELATIVE_TO_SELF, 0, Animation.RELATIVE_TO_SELF, 0, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0));
+                                welcomeText.startAnimation(textAnimationSet);
+                            }
+                        } else {
+                            welcomeShowed = false;
+
+                        }*/
+                    }else{
+                        if(loginHolderStatus== LOGINHOLDER_STATUS_OUT) {
+                            loginHolderStatus = LOGINHOLDER_STATUS_VISIBLE;
+                            loginHolder.animate().y(0).setDuration(200).setStartDelay(0).start();
+                        }
+                    }
+                    mainHided= false;
                 }
 
-
+                //paralax.scrollTo(0, (int) (paralax.getHeight()*(page/10)));
+                paralaxImage.animate().y(-(((float) paralax.getHeight()) * ((float) page / 10))).setDuration(0).setStartDelay(0).start();
 
             }
         });
--- app/src/main/java/im/actor/tour/TourFragment.java
+++ app/src/main/java/im/actor/tour/TourFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import im.actor.tour.R;
 
@@ -13,6 +15,7 @@ import im.actor.tour.R;
  * Created by Jesus Christ. Amen.
  */
 public class TourFragment extends Fragment {
+    private static final String ARG_POSITION = "arg_pos";
     private View rootView;
 
     @Override
@@ -20,6 +23,40 @@ public class TourFragment extends Fragment {
 
         rootView = inflater.inflate(R.layout.fragment_tour, null);
 
+        TextView titleView = (TextView) rootView.findViewById(R.id.title);
+        TextView bodyView = (TextView) rootView.findViewById(R.id.body);
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.image);
+
+
+        Bundle args = getArguments();
+        int position = args.getInt(ARG_POSITION);
+
+        switch (position) {
+            case 1:
+                titleView.setText("Hudge Group Chats");
+                bodyView.setText("Incredibly large group chats. More than 100 users in one group. Communicate with your team and make decisions together.");
+                imageView.setImageResource(R.drawable.intro_groups);
+                break;
+            case 2:
+                titleView.setText("Works everywhere");
+                bodyView.setText("No matter where you are in the subway or on the mountain hills. Your message will always reach the right people.");
+                imageView.setImageResource(R.drawable.intro_subway);
+                break;
+            case 3:
+                titleView.setText("Secure & Private");
+                bodyView.setText("Rest assured no one will read your messages. Share documents, files and photos without fear that they could fall into the wrong hands.");
+                imageView.setImageResource(R.drawable.intro_secure);
+                break;
+        }
+
         return rootView;
+    }
+
+    public static Fragment getInstance(int position) {
+        Fragment fragment = new TourFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_POSITION,position);
+        fragment.setArguments(args);
+        return fragment;
     }
 }
